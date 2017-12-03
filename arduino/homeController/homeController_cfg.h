541a5f96731ed6725e6bfb911660ef076b048b60
stefan <mail1@meek.de>
2017-12-03 16:20:24 +0100

    fixed missing "

--- arduino/homeController/homeController_cfg.h
+++ arduino/homeController/homeController_cfg.h
@@ -3,7 +3,7 @@
 
 const char* WLAN_SSID = "xxx";
 const char* WLAN_PASSWORD =  "xxx";
-const char* MQTT_SERVER = xxx";
+const char* MQTT_SERVER = "xxx";
 const int MQTT_PORT = 123;
 const char* MQTT_USER = "xxx";
 const char* MQTT_PASSWORD = "xxx";
